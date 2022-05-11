d656dd6008a6400e49f6ba3663ed7a755b949f34
DivyantAgarwal <40781887+DivyantAgarwal@users.noreply.github.com>
2022-05-11 19:10:16 +0530

    Doing first commit, uploading basic switch

---
+++ SwitchDemo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SwitchDemo{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a number between 0 to 2");
+		int num = in.nextInt();
+		switch(num){
+			case 0:System.out.println("Sunday");break;
+			case 1:System.out.println("Monday");break;
+			default : System.out.println("Not a valid num");
+		}
+
+	}
+}
